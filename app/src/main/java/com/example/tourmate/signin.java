719dde1a940d5e7a6605a61b7815ea3cf7a043c6
Rafsun-Trex <61016162+Rafsun-Trex@users.noreply.github.com>
2022-02-24 23:51:02 +0600

    firebase done 2

--- app/src/main/java/com/example/tourmate/signin.java
+++ app/src/main/java/com/example/tourmate/signin.java
@@ -1,8 +1,5 @@
 package com.example.tourmate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,9 +7,11 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -71,7 +70,12 @@ public class signin extends AppCompatActivity {
                         startActivity(new Intent(signin.this,welcome.class));
                         finish();
                     }
-                });
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(signin.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
