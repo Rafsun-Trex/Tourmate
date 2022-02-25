b7d77035218c3aa0424e85863bc3370dde3a28d9
Rafsun-Trex <61016162+Rafsun-Trex@users.noreply.github.com>
2022-02-25 17:54:06 +0600

    firebase done 3

--- app/src/main/java/com/example/tourmate/MainActivity.java
+++ app/src/main/java/com/example/tourmate/MainActivity.java
@@ -1,24 +1,21 @@
 package com.example.tourmate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Handler h = new Handler();
-    FirebaseAuth auth;
+    //FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
+        //auth = FirebaseAuth.getInstance();
 
         h.postDelayed(new Runnable() {
             @Override
@@ -31,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
         },5000);
     }
 
-    @Override
+   /* @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser user = auth.getCurrentUser();
         if(user == null){
             startActivity(new Intent(MainActivity.this, registration.class));
         }
-    }
+    } */
 }
